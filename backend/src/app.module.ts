d74798189351d40b740105380cfcc2ee4a442446
unsean <unseangt@gmail.com>
2026-03-03 15:53:31 +0700

    fix appmodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { IdentityModule } from './identity/identity.module';
 import { IdentityModule } from './modules/identity/identity.module';
-import { LedgerModule } from './modules/ledger/ledger.module';
-import { LedgarModule } from './modules/ledgar/ledgar.module';
+import { LedgerModule } from './modules/ledger/ledger.module'
 
 @Module({
-  imports: [IdentityModule, LedgerModule, LedgarModule],
+  imports: [IdentityModule, LedgerModule],
   controllers: [AppController],
   providers: [AppService],
 })
