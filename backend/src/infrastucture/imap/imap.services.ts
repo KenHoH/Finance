8f48cbdd73ece31e2dd9875209089d7f81ae3a5c
kenneth <nethk79@gmail.com>
2026-03-17 12:59:29 +0700

    feat: blu, ovo, BCA email parser

--- backend/src/infrastucture/imap/imap.services.ts
+++ backend/src/infrastucture/imap/imap.services.ts
@@ -1,52 +1,8 @@
 import { ImapFlow } from "imapflow";
 import { simpleParser } from 'mailparser';  
-import * as cheerio from "cheerio";
-
-
-function parseIDRCurrency(value: string): number {
-  return Number(
-    value
-      .replace(/^Rp\s?/, '')
-      .replace(/\./g, '')
-      .replace(',', '.')
-      .trim()
-  );
-}
-
-function extractInfoFromHTMLBLU(html: string) {
-    const $ = cheerio.load(html);
-
-    // Unused Filter (16/03/2026)
-    // const amounts = $('span')
-    // .filter((i, el) => $(el).text().trim() === 'Rp')
-    // .map((i, el) => $(el).next().text().trim())
-    // .get();
-    // const splitDetails = bankDetailsHtml.split(/<br\s*\/?>/i);
-    // const bankNameRaw = splitDetails[0] ? splitDetails[0].trim() : "Bank not found";
-    // const accountRaw = splitDetails[1] ? splitDetails[1].trim() : "Account not found";
-    // const bankDetailsHtml = $('span[style*="#8993A4"]').html() || '';
-
-    const allNames = $('span[style*="padding-top:8px"]')
-    .map((i, el) => $(el).text().trim())
-    .get();
-
-    const receipentName = allNames[0] || "Name not found";
-    const senderName = allNames[1] || "Name not found";
-    const names = `Sender: ${senderName}, Recipient: ${receipentName}`;
-
-    const labelDate = $('div').filter((i, el) => $(el).text().trim() === 'Tgl & Jam Transaksi');
-    const dateTimeRaw = labelDate.closest('div[class*="mj-column"]').next('div[class*="mj-column"]').text().trim();
-
-    const labelTotal = $('div').filter((i, el) => $(el).text().trim() === 'Total');
-    let totalAmountRaw = labelTotal.closest('div[class*="mj-column"]').next('div[class*="mj-column"]').text().trim();
-    totalAmountRaw = totalAmountRaw.replace(/^Rp/, '').trim();
-    const totalAmount = parseIDRCurrency(totalAmountRaw);
-
-    console.log("TRANSACTION DETAILS:");
-    console.log(`Name:    ${names}`);      
-    console.log(`Date:    ${dateTimeRaw}`);
-    console.log(`Total:   Rp ${totalAmount.toFixed(2)}`); 
-}
+import { extractInfo } from "./helper/extractInfo.js";
+import * as fs from 'fs';
+import { AuthError, ConnectionError } from "../errors/error.js";
 
 export async function connectToImap(userEmail: string, googleAccessToken: string) {
 
@@ -62,6 +18,7 @@ export async function connectToImap(userEmail: string, googleAccessToken: string
     });
 
     try {
+        // Connect to the IMAP server
         await client.connect();
 
         // Label "Important Stuff" because i filterize the important email like OVO, BLU and BCA into this label
@@ -70,7 +27,6 @@ export async function connectToImap(userEmail: string, googleAccessToken: string
 
         client.on("exists", async (data) => {
             console.log(`\nNew email arrived! Total in Inbox: ${data.count}`);
-
             const sequenceRange = `${data.prevCount + 1}:*`;
 
             try {
@@ -84,11 +40,9 @@ export async function connectToImap(userEmail: string, googleAccessToken: string
                     console.log("HTML:", parsed.html);
                     console.log("-----------------------------------");
 
-                    if(parsed.html){
-                        extractInfoFromHTMLBLU(parsed.html || '');
-                    }else {
-                        console.log("No HTML content found in the email. Skipping extraction.");
-                    }
+                    let result = extractInfo(parsed.subject, parsed.from.text, parsed.html)
+
+                    console.log("Extracted Info:", result);
                 }
             } catch (err) {
                 console.error("Error fetching new message:", err);
@@ -96,6 +50,14 @@ export async function connectToImap(userEmail: string, googleAccessToken: string
         });
 
     } catch (err) {
-        console.error('IMAP Connection Error:', err);
+        // Generate access token google
+        if (err.authenticationFailed) {
+            throw new AuthError();
+        }
+
+        if (err.code === 'NoConnection') {
+            throw new ConnectionError();
+        }
+        throw err;
     }
 }
