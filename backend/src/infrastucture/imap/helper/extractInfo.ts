7a227eace85019255477a690e50b4832ec72b746
kenneth <nethk79@gmail.com>
2026-03-17 12:59:29 +0700

    feat: blu, ovo, BCA email parser

---
+++ backend/src/infrastucture/imap/helper/extractInfo.ts
@@ -0,0 +1,32 @@
+import { extractInfoFromHTMLBCA } from "./parseEmail/helperBCA.js";
+import { extractInfoFromHTMLBLU } from "./parseEmail/helperBLU.js";
+import { extractInfoFromHTMLOVO } from "./parseEmail/helperOVO.js";
+
+
+export interface ExtractedInfo {
+    status: boolean;
+    amount: number;
+    date: string;
+    recipient: string;
+}
+
+export function extractInfo(subject: string, sender: string, html:string) : ExtractedInfo {
+
+
+    if(subject === 'Transaksimu Pakai blu Berhasil'){
+        return extractInfoFromHTMLBLU(html);
+    }
+    else if(subject === 'Internet Transaction Journal'){
+        return extractInfoFromHTMLBCA(html);
+    }
+    else if(subject === 'OVO QR Payment Receipt'){
+        return extractInfoFromHTMLOVO(html);
+    }
+
+    return {
+        status: false,
+        amount: 0,
+        date: "",
+        recipient: ""
+    };
+}
